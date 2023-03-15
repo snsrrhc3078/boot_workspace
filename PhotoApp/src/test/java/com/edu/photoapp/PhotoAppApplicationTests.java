8fe31b8c6423b7fda6360b87a99959b17ddf7a36
snsrrhc3078 <snsrrhc3078@gmail.com>
2023-03-15 13:52:06 +0900

    [feat] 20230315 강의 프로젝트
    스프링boot랑 앱 연동해서 스마트폰으로 찍은 이미지 스프링 부트 게시판에
    올리기 and 스마트폰이랑 스프링 부트 서버 웹소켓 이용해서 실시간으로
    통신하는 예제 1

---
+++ PhotoApp/src/test/java/com/edu/photoapp/PhotoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.photoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
